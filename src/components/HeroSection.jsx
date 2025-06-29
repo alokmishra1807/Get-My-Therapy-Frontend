153a5959c6a21d3d3ad2ccdb5ec452f1af709caa
alokmishra <alok.230101016@iiitbh.ac.in>
2025-06-30 01:36:52 +0530

    button added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,13 +7,18 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <>
-    <Image 
-  src="/ban.jpg"
-  alt="Banner"
-  width={1200}
-  height={600}
-  className="w-full h-auto object-cover hidden md:block"
-/>
+   <div className="relative hidden md:block">
+        <Image 
+          src="/ban.jpg"
+          alt="Banner"
+          width={1200}
+          height={600}
+          className="w-full h-auto object-cover"
+        />
+        <button className="absolute bottom-6 cursor-pointer left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 text-black text-lg px-6 py-2 rounded-lg hover:bg-blue-700 transition">
+          Book Appointment
+        </button>
+      </div>
     <Image 
   src="/ban.jpg"
   alt="Banner"
